e0e6e80862f0d24417f2abe73985832ec0efbd34
Knightblazer <tasogareika@gmail.com>
2019-11-27 17:23:59 +0800

    Did randomized spawning for buttons.

--- Assets/Scripts/WordSorterHandler.cs
+++ Assets/Scripts/WordSorterHandler.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WordSorterHandler : MonoBehaviour
 {
     public static WordSorterHandler singleton;
     public static GameObject currButton, currList;
+    public static int roundNum;
     public Camera mainCam;
     public Canvas mainCanvas;
     public GameObject leftColumn, rightColumn;
@@ -27,6 +29,7 @@ public class WordSorterHandler : MonoBehaviour
     {
         currButton = null;
         currList = null;
+        roundNum = 1;
 
         filePath = Application.dataPath + "/" + "wordData.txt";
         readFile();
@@ -50,8 +53,83 @@ public class WordSorterHandler : MonoBehaviour
     {
         System.Random ran = new System.Random();
         int index = ran.Next(1, wordList.Count);
+        buttonSpawn();
         spawnWords(wordList[index]);
         wordList.RemoveAt(index);
+    }
+
+    private void buttonSpawn() //randomize starting placement
+    {
+        foreach (var b in buttonList)
+        {
+            b.transform.SetParent(mainCanvas.transform);
+            b.transform.SetSiblingIndex(1);
+            b.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+            b.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+        }
+        
+        float rightside = appWidth * 0.5f;
+        float midpoint = appHeight * 0.45f;
+
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            float xVariance = Mathf.RoundToInt(Random.Range(0, appWidth * 0.3f));
+            float yVariance = Mathf.RoundToInt(Random.Range(0, appHeight * 0.1f));
+
+            switch (i)
+            {
+                case 0:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(0 + xVariance, midpoint - yVariance);
+                    break;
+
+                case 1:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(0 + xVariance, midpoint * 0.7f - yVariance);
+                    break;
+
+                case 2:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(0 + xVariance, midpoint * 0.4f - yVariance);
+                    break;
+
+                case 3:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(0 + xVariance, midpoint * 0.1f - yVariance);
+                    break;
+
+                case 4:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(rightside + xVariance, midpoint - yVariance);
+                    break;
+
+                case 5:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(rightside + xVariance, midpoint * 0.7f - yVariance);
+                    break;
+
+                case 6:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(rightside + xVariance, midpoint * 0.4f - yVariance);
+                    break;
+
+                case 7:
+                    buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(rightside + xVariance, midpoint * 0.1f - yVariance);
+                    break;
+            }
+
+            //shift back to edge if crossed boundaries
+            if (buttonList[i].GetComponent<RectTransform>().anchoredPosition.y < 0)
+            {
+                buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(buttonList[i].GetComponent<RectTransform>().anchoredPosition.x, 0);
+            }
+
+            if (buttonList[i].GetComponent<RectTransform>().anchoredPosition.x > appWidth - buttonList[i].GetComponent<RectTransform>().rect.width)
+            {
+                buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(appWidth - buttonList[i].GetComponent<RectTransform>().rect.width, buttonList[i].GetComponent<RectTransform>().anchoredPosition.y);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     private void spawnWords(string line)
@@ -98,6 +176,12 @@ public class WordSorterHandler : MonoBehaviour
         if (leftColumn.transform.parent.GetComponent<ListHander>().allCorrect() && rightColumn.transform.parent.GetComponent<ListHander>().allCorrect())
         {
             Debug.Log("all correct!");
+            roundNum++;
+            if (roundNum < 4)
+            {
+                chooseCategory();
+            }
+
         } else
         {
             Debug.Log("try again");
